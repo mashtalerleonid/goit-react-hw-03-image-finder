3181d078ba01a90a23b2dcc9e7b94a80bccf3d9f
mashtalerleonid <mashtalerleonid@gmail.com>
2021-09-15 20:34:47 +0300

    scroll-lock

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,14 +3,17 @@ import { createPortal } from "react-dom";
 import { ReactComponent as CloseIcon } from "../../icons/close.svg";
 
 const modalRoot = document.querySelector("#modal-root");
+const body = document.querySelector("body");
 
 class Modal extends Component {
   componentDidMount() {
     window.addEventListener("keydown", this.handleKeyDown);
+    body.classList.add("scroll-lock");
   }
 
   componentWillUnmount() {
     window.removeEventListener("keydown", this.handleKeyDown);
+    body.classList.remove("scroll-lock");
   }
 
   handleKeyDown = (e) => {
